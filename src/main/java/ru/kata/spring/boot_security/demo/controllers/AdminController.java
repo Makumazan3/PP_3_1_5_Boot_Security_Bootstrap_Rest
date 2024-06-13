a864063c1fa9b3e67e5df0df3d525dedc544d87d
Konstantin Bondarenko <konstantin-bond@yandex.ru>
2024-06-13 14:50:35 +0300

    Я сделал задачу.

--- src/main/java/ru/kata/spring/boot_security/demo/controllers/AdminController.java
+++ src/main/java/ru/kata/spring/boot_security/demo/controllers/AdminController.java
@@ -1,9 +1,10 @@
 package ru.kata.spring.boot_security.demo.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.*;
 import ru.kata.spring.boot_security.demo.models.Role;
 import ru.kata.spring.boot_security.demo.models.User;
@@ -11,9 +12,8 @@ import ru.kata.spring.boot_security.demo.services.MyDetailsService;
 import ru.kata.spring.boot_security.demo.services.RoleService;
 import ru.kata.spring.boot_security.demo.services.UserService;
 
-import java.util.HashSet;
+import java.util.ArrayList;
 import java.util.List;
-import java.util.Set;
 
 @Controller
 @RequestMapping("/admin")
@@ -21,63 +21,70 @@ public class AdminController {
     private final MyDetailsService myDetailsService;
     private final UserService userService;
     private final RoleService roleService;
-@Autowired
+
+    @GetMapping(value = "/")
+    public String login() {
+
+        return "redirect:/login";
+    }
+
+    @GetMapping("/logout")
+    public String logoutPage() {
+        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+        if (auth != null) {
+            SecurityContextHolder.getContext().setAuthentication(null);
+        }
+
+        return "redirect:/login?logout";
+    }
+
+    @Autowired
     public AdminController(MyDetailsService myDetailsService, UserService userService, RoleService roleService) {
-    this.myDetailsService = myDetailsService;
-    this.userService = userService;
-    this.roleService = roleService;
+        this.myDetailsService = myDetailsService;
+        this.userService = userService;
+        this.roleService = roleService;
     }
 
 
     // вывод всех юзеров
-@GetMapping()
-public String showAllUsers(ModelMap model){
+    @GetMapping()
+    public String showAllUsers(Model model, Authentication authentication) {
+        String userName = authentication.getName();
+        User user = (User) myDetailsService.loadUserByUsername(userName);
+        model.addAttribute("addRoles", userService.showAllUser());
+        model.addAttribute("userInfo", user);
         model.addAttribute("users", userService.showAllUser());
+        model.addAttribute("newUser", new User());
+        model.addAttribute("setRoles", roleService.getAllRoles());
+
         return "users";
-}
+    }
 
 //создание нового юзера
-    @GetMapping("/new")
-    public String createUser(Model model) {
-        model.addAttribute("user", new User());
-        model.addAttribute("role", new Role());
-        return "new";
-    }
 
     @PostMapping("/saveUser")
     public String save(@ModelAttribute("user") User user,
-                       @RequestParam(name = "roles", required = true) Set<Long> roleIds) {
-   Set<Role> roles = new HashSet<>();
-    for (Long roleId : roleIds){
-        roles.add(roleService.getRoleById(roleId));
-    }
+                       @RequestParam(name = "roles", required = true) List<Long> roleIds) {
+        List<Role> roles = new ArrayList<>();
+        for (Long roleId : roleIds) {
+            roles.add(roleService.getRoleById(roleId));
+        }
+        user.setRoles(roles);
         userService.addUser(user);
         return "redirect:/admin";
     }
     //    обновление юзера
 
-    @GetMapping("/edit")
-    public String editUser(Model model, @RequestParam Long id) {
-        model.addAttribute("user", userService.getUserById(id));
-        model.addAttribute("userList", userService.showAllUser());
-        return "edit";
-    }
-    @PostMapping("/updateUser")
-    public String updateUser(@RequestParam("id") Long id,
-                             @ModelAttribute("user") User user,
-                             @RequestParam(name = "roles", required = true) List<Long> roleIds) {
-        User user1 = userService.getUserById(id);
-        user1.setName(user.getName());
-        user1.setPassword(user.getPassword());
-        user1.setEmail(user.getEmail());
-        userService.updateUser(id, user1);
+    @PutMapping("/edit")
+    public String update(@PathVariable("id") Long id, @ModelAttribute User user) {
+        userService.updateUser(id, user);
         return "redirect:/admin";
     }
 
 
-//    удаление юзера
-    @PostMapping("/delete")
-    public String delete(@RequestParam(value = "id") Long id) {
+    //    удаление юзера
+    @DeleteMapping(value = "/delete/{id}")
+    public String delete(@PathVariable("id") Long id) {
         userService.deleteUser(id);
         return "redirect:/admin";
     }
